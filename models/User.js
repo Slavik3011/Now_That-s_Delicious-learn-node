f8cfffa8eaa899b630ea87636c39d6f3f29d31d2
Slavik3011 <Slavik3011@yandex.ru>
2018-03-03 20:10:52 +0200

    create user accounts

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+const md5 = require('md5');
+const validator = require('validator');
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, 'Invalid Email Address'],
+    required: 'Please Supply an email address'
+  },
+  name: {
+    type: String,
+    required: 'Please supply a name',
+    trim: true
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
+});
+
+userSchema.virtual('gravatar').get(function() {
+  // return 'https://openclipart.org/image/2400px/svg_to_png/277084/Male-Avatar-3.png'
+  const hash = md5(this.email);
+  return `https://gravatar.com/avatar/${hash}?s=200`
+});
+
+userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
+userSchema.plugin(mongodbErrorHandler);
+
+module.exports = mongoose.model('User', userSchema);
